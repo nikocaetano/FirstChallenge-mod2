fc7bb8cd625608b51abb97b5b2e1246a312064ea
niko caetano <ellercaetano@gmail.com>
2022-06-10 16:28:29 -0300

    primeiro desafio do módulo2

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = 'Nikolas'
+console.log(nome)
+
+let number = 24
+console.log(number)
+
+let ano = 2022 - number
+console.log(ano)
+console.log(number + ano)
+
+let string = "Estou mexendo no console.log"
+console.log(string)
+
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
